cd8d8fdecf8892a2f7d7dc821a458dc489713bff
G <geconghuii@outlook.com>
2021-12-20 15:21:49 +0800

    Empty project

---
+++ AttackerLab/src/main/java/com/InternetworkingProject/AttackerLab/AttackerLabApplication.java
@@ -0,0 +1,13 @@
+package com.InternetworkingProject.AttackerLab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttackerLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttackerLabApplication.class, args);
+	}
+
+}
